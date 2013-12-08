361a557e64c96a0a71e6802cc88bd56794b7f9e3
Juergen Gerharz <juergen.gerharz@inisnet.de>
2013-12-08 09:03:47 +0100

    Laufzeit Variable wird aktualisiert wenn vorhanden

--- IPSLibrary/app/hardware/Plugwise/Plugwise_Controller.ips.php
+++ IPSLibrary/app/hardware/Plugwise/Plugwise_Controller.ips.php
@@ -396,7 +396,9 @@ function plugwise_0013_received($buf)
 		$telegramm_counter = $telegramm_counter + 1;
 		}
    SetValue(IPS_GetVariableIDByName ("LastMessage", $myCat),$diff.";".$telegramm_counter);
-   //SetValue(@IPS_GetVariableIDByName ("Laufzeit", $myCat),$diff);
+	$LaufzeitID = @IPS_GetVariableIDByName ("Laufzeit", $myCat);
+	if ( $LaufzeitID )
+   	SetValue($LaufzeitID,$diff);
 
 	//IPS_LogMessage("Info","[".$string."]".$diff);
 	//*********************************************
