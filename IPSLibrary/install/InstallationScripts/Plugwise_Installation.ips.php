e281be8e0254bd0a88f63899dbd1b3c731b53c4f
1007 <juergen.gerharz@inisnet.de>
2015-12-28 11:23:16 +0100

    Ohne Kommpatibel

--- IPSLibrary/install/InstallationScripts/Plugwise_Installation.ips.php
+++ IPSLibrary/install/InstallationScripts/Plugwise_Installation.ips.php
@@ -45,6 +45,8 @@
 	IPSUtils_Include ("Plugwise_Include.ips.php",      "IPSLibrary::app::hardware::Plugwise");
 	IPSUtils_Include ("Plugwise_Profile.inc.php",      "IPSLibrary::config::hardware::Plugwise");
 
+  $KompatibelFile = IPS_GetKernelDir()."scripts/IPSLibraryapp/hardware/Plugwise/Plugwise_Once.ips.php";
+  require_once($KompatibelFile);
 
 
 
@@ -818,100 +820,6 @@
     echo "\n<br>";
     }
     
-	/** Anlegen einer IO Instanze mit seriellem Port
-	 *
-	 * Die Funktion legt eine Serielle IO Instanze an. Wenn unter der ParentId bereits eine Instanze mit selbem
-	 * Namen existiert, werden alle Parameter auf den aktuellen Wert gesetzt.
-	 *
-	 * @param string $Name Name der IO Instanze
-	 * @param string $ComPort Name des Com Ports
-	 * @param integer $Baud Baud Rate des seriellen Ports
-	 * @param integer $StopBits Einstellung Stop Bits des seriellen Ports
-	 * @param integer $DataBits Parity Data Bits des seriellen Ports
-	 * @param string $Parity Parity Einstellung des seriellen Ports
-	 * @param integer $Position Positions Wert des Objekts
-	 * @param boolean $IgnoreError Ignoriren von Fehlern bei der Generierung der Instanz, andernfalls wird das Script abgebrochen
-	 * @return integer ID der seriellen Port Instanze
-	 *
-	 */
-	function CreateSerialPortNew($Name, $ComPort, $Baud=9600, $StopBits=1, $DataBits=8, $Parity='None', $Position=0, $IgnoreError=true)
-		{
-
-		$InstanceID = CreateInstance($Name, 0, "{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}",$Position);
-		IPS_SetProperty($InstanceID, 'BaudRate', $Baud);
-		IPS_SetProperty($InstanceID, 'StopBits', $StopBits);
-		IPS_SetProperty($InstanceID, 'DataBits', $DataBits);
-		IPS_SetProperty($InstanceID, 'Parity', $Parity);
-		IPS_SetProperty($InstanceID, 'Port', $ComPort);
-		IPS_SetProperty($InstanceID, 'Open', true);
-
-		if (!@IPS_ApplyChanges($InstanceID) and !$IgnoreError) {
-			Error ("Error applying Changes to ComPort Instance --> Abort Script");
-		};
-		return $InstanceID;
-	}
-
-
-if (!function_exists('Cutter_SetParseType'))
-{	
-	function Cutter_SetParseType($InstanceID, $_Type)
- 	{
-		IPS_SetProperty($InstanceID, 'ParseType', $_Type);
-	}
-}
-
-if (!function_exists('Cutter_SetLeftCutChar'))
-{
-	function Cutter_SetLeftCutChar($InstanceID, $Value)
- 	{
-		IPS_SetProperty($InstanceID, 'LeftCutChar', $Value);
-	}
-}
-
-if (!function_exists('Cutter_SetRightCutChar'))
-{
-	function Cutter_SetRightCutChar($InstanceID, $Value)
- 	{
-		IPS_SetProperty($InstanceID, 'RightCutChar', $Value);
-	}
-}
-	
-if (!function_exists('Cutter_SetTimeout'))
-{
-	function Cutter_SetTimeout($InstanceID, $Milliseconds)
- 	{
-		IPS_SetProperty($InstanceID, 'Timeout', $Milliseconds);
-	}
-}
-
-
-if (!function_exists('IPS_SetEventCyclicTimeBounds')) {
-	
-	function IPS_SetEventCyclicTimeBounds($EventID, $FromTime, $ToTime)
-	{
-		$ret = true;
-		if($FromTime == 0) {
-			$ret = $ret & IPS_SetEventCyclicTimeFrom($EventID, 0, 0, 0);
-		} else {
-			$ret = $ret & IPS_SetEventCyclicTimeFrom($EventID, (int)date("H", $FromTime), (int)date("i", $FromTime), (int)date("s", $FromTime));
-		}
-		if($ToTime == 0) {
-			$ret = $ret & IPS_SetEventCyclicTimeTo($EventID, 0, 0, 0);
-		} else {
-			$ret = $ret & IPS_SetEventCyclicTimeTo($EventID, (int)date("H", $ToTime), (int)date("i", $ToTime), (int)date("s", $ToTime));
-		}
-		return $ret;
-	}
-}
-
-  
-if (!function_exists('RegVar_SetRXObjectID'))
-{  
-	function RegVar_SetRXObjectID($InstanceID, $ObjectID)
- 	{
-		IPS_SetProperty($InstanceID, 'RXObjectID', $ObjectID);
-	}
-}
     
 //******************************************************************************
 //	Umlaute ersetzen
